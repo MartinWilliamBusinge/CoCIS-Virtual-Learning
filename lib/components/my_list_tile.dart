826e80824168586cfa38d539f5686c642408ef5d
jescanishacode <jessicanakirijja90@gmail.com>
2024-07-09 06:44:17 -0700

    jesca

--- lib/components/my_list_tile.dart
+++ lib/components/my_list_tile.dart
@@ -22,7 +22,7 @@ class MyListTile extends StatelessWidget {
         onTap: onTap ,
         title: Text(
           text,
-          style: TextStyle(color: Colors.white),
+          style: const TextStyle(color: Colors.white),
         ),
       ),
     );
